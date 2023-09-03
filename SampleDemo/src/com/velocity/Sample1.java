1a4d31caac910a2a249942f0b62ea716143476ba
PrajaktaJagdale97github <ppjagdale97@gmail.com>
2023-09-03 18:29:48 +0530

    added gitignore files

--- SampleDemo/src/com/velocity/Sample1.java
+++ SampleDemo/src/com/velocity/Sample1.java
@@ -1,5 +1,11 @@
 package com.velocity;
 
 public class Sample1 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			System.out.println("The value of i is>>"+i);
+		}
+		
+	}
 
 }
